2d66a9cdbcc946f9bf33c6e88e141cb3cef65451
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-06-20 22:50:58 -0400

    Added users and reviews routes

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const users = require("../data/users");
+
+router.get("/", (req, res) => {
+  res.json(users);
+});
+
+router.get("/:id", (req, res) => {
+  const user = users.find(
+    user => user.id === Number(req.params.id)
+  );
+
+  if (!user) {
+    return res.status(404).json({
+      error: "User not found"
+    });
+  }
+
+  res.json(user);
+});
+
+module.exports = router;
